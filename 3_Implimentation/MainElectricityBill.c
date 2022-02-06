26de8f7e230c69e9ac979fac05fe90e9e1888c79
sankethgowda06 <82862192+sankethgowda06@users.noreply.github.com>
2022-02-06 23:00:08 +0530

    Update MainElectricityBill.c

--- 3_Implimentation/MainElectricityBill.c
+++ 3_Implimentation/MainElectricityBill.c
@@ -2,25 +2,24 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
-#include<Windows.h>
 
 
 void details()
 {
-    printf("Enter your  area name:\n");
-	scanf("%s", B.area);
-	printf("Enter your  name:\n");
-	scanf("%s", B.name);
-	printf("Enter your Meter Number:\n");
-	scanf("%d", &B.meternumber);
-    printf("Enter units consumed:\n");
-	scanf("%d", &B.unitsconsumed);
-    printf("Enter email ID :\n");
-    scanf("%s", B.email);
-    printf("Enter the permanent address :\n");
-    scanf("%s", B.address);
-    printf("Enter the phone number : \n");
-    scanf("%s", B.phonenumber);
+printf("Enter your  area name:\n");
+scanf("%s", B.area);
+printf("Enter your  name:\n");
+scanf("%s", B.name);
+printf("Enter your Meter Number:\n");
+scanf("%d", &B.meternumber);
+printf("Enter units consumed:\n");
+scanf("%d", &B.unitsconsumed);
+printf("Enter email ID :\n");
+scanf("%s", B.email);
+printf("Enter the permanent address :\n");
+scanf("%s", B.address);
+printf("Enter the phone number : \n");
+scanf("%s", B.phonenumber);
 }
 
 void Urban()
