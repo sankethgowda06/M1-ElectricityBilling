e80cb003543a674030718ac2619ecfebd1f1c8d2
sanketh Gowda A <sankethgowda06@gmail.com>
2022-02-11 13:00:28 +0530

    main

---
+++ 3_Implementation/test/test_hms.c
@@ -0,0 +1,23 @@
+#include "unity.h"
+#include <functions.h>
+
+#define PROJECT_NAME "hospital"
+
+
+void setUp(){}
+
+void tearDown(){}
+
+void test_exitt(void){
+  TEST_ASSERT_EQUAL_STRING("EXITING THE PROGRAM",exitt());
+    
+}
+int main()
+{
+
+  UNITY_BEGIN();
+
+
+  RUN_TEST(test_exitt);
+  return UNITY_END();
+}
